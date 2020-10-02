f0835d69b2afa8bf2c92371420080e7f496dfd2a
gskim <gskim@DESKTOP-7IB7F4H>
2020-10-02 14:14:33 +0900

    Add repeat function

--- src/gskim/calendar/Calendar.java
+++ src/gskim/calendar/Calendar.java
@@ -3,17 +3,38 @@ package gskim.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	public static void main(String[] args) {
-		System.out.println("두 수를 입력하세요 : ");
+	public static void main(String[] args) {		
+		System.out.println("반복 횟수를 입력하세요 : ");
 		Scanner scanner = new Scanner(System.in);
-		String s1 = scanner.next();
-		String s2 = scanner.next();
+		int repeat = scanner.nextInt();
 		
-		int first = Integer.parseInt(s1);
-		int second = Integer.parseInt(s2);
+		int[] month = new int[repeat];
+		for(int i=0; i<repeat; i++){
+			System.out.println((i+1)+"번째 달을 입력하세요 : ");
+			month[i] = scanner.nextInt();
+		}
 		
-		System.out.println("두 수의 합은 " + (first+second) + "입니다.");
+		int[] monthDay = new int[3];
+		monthDay[0] = 31;
+		monthDay[1] = 30;
+		monthDay[2] = 28;
+		
+		for(int j=0; j<repeat; j++){
+			switch(month[j]){
+			case 1 : case 3 : case 5 : case 7 : case 8 : case 10 : case 12 :
+				System.out.println(month[j]+"월은 "+ monthDay[0] +"일까지 있습니다.");
+				break;
+			case 4 : case 6 : case 9 : case 11 : 
+				System.out.println(month[j]+"월은 "+ monthDay[1] +"일까지 있습니다.");
+				break;
+			case 2 :
+				System.out.println(month[j]+"월은 "+ monthDay[2] +"일까지 있습니다.");
+				break;
+			default :
+				System.out.println("1월부터 12월 사이의 숫자를 입력하세요.");
+				break;
+			}		
+		}
 		scanner.close();
 	}
-
 }
