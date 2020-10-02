7214592bff383fb24506262f40f17fffe05e7230
gskim <gskim@DESKTOP-7IB7F4H>
2020-10-02 13:37:39 +0900

    Add sum of two numbers

--- src/gskim/calendar/Calendar.java
+++ src/gskim/calendar/Calendar.java
@@ -1,13 +1,19 @@
 package gskim.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println(" 일	 월	 화	 수	 목	 금	 토");
-		System.out.println("---------------------------------------------------");
-		System.out.println(" 1	 2	 3	 4	 5	 6	 7");
-		System.out.println(" 8	 9	10	11	12	13	14");
-		System.out.println("15	16	17	18	19	20	21");
-		System.out.println("22	23	24	25	26	27	28");
+		System.out.println("두 수를 입력하세요 : ");
+		Scanner scanner = new Scanner(System.in);
+		String s1 = scanner.next();
+		String s2 = scanner.next();
+		
+		int first = Integer.parseInt(s1);
+		int second = Integer.parseInt(s2);
+		
+		System.out.println("두 수의 합은 " + (first+second) + "입니다.");
+		scanner.close();
 	}
 
 }
